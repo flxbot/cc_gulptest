8616e1d2b3f69679236e1a4f00ff1c569e1acabd
flxbot <flxbot@gmail.com>
2019-11-12 17:32:02 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,15 @@ var rename = require("gulp-rename");
 
 gulp.task('css', function() {
     var plugins = [];
-    return gulp.src('./_site/css/cyxc.css')
+    return gulp.src('./_site/css/main.css')
         .pipe(postcss([
             uncss({
                 html: ['_site/**/*.html'],
                 htmlroot: '_site',
-                ignore: ['.monthholder', '.col-6', '.col-sm-4', '.col-lg-3', '.ex-month', '.month', '.unavailable']
+                ignore: ['.red']
             }),
             cssnano()
         ]))
         .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest('./_site/css/'));
 });
-
-
-
-
- 
---
+++ index.markdown
@@ -0,0 +1,15 @@
+---
+title: Home
+layout: default
+---
+
+
+<h1 class="blue">Blue Heading</h1>
+<p class="green">Green Text</p>
+<p class="violet">Red Text</p>
+<h2>Heading 2</h2>
+<p class="green">Green Text</p>
+<p class="violet">Red Text</p>
+<h2>Heading 2</h2>
+<p class="green">Green Text</p>
+<p class="green">Green Text</p>
