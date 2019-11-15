17aa0fbfc2d1d543d5cea9540334820c2fd03d3b
flxbot <flxbot@gmail.com>
2019-11-15 14:54:06 +0100

    asadfsd

--- _plugins/hooks/gulpcss.rb
+++ _plugins/hooks/gulpcss.rb
@@ -1,3 +1,3 @@
-Jekyll::Hooks.register(:site, :post_render) do ||
+Jekyll::Hooks.register(:site, :post_write) do |_site|
   system("./node_modules/.bin/gulp css")
 end
--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,21 @@ var cssnano = require("cssnano"); // CSS Minifier
 var uncss = require('postcss-uncss');
 var rename = require("gulp-rename");
 var fs = require('fs');
-var child = require('child_process');
-var spawn = require('cross-spawn');
-var debug = require('gulp-debug');
-
 
 gulp.task('css', function() {
     var plugins = [];
-   // fs.writeFileSync('test.txt', 'hello world');
+    fs.writeFileSync('test.txt', 'hello world');
     return gulp.src('./css/main.css')
         .pipe(postcss([
             uncss({
-                html: ['test.html'],
+                html: ['index.html'],
                 htmlroot: '_site',
-                ignore: ['.gray']
+                ignore: ['.green']
             }),
             cssnano()
         ]))
-                .pipe(debug({title: 'gulp-debug:'}))
-
         .pipe(rename({ suffix: ".min" }))
-                .pipe(debug({title: 'gulp-debug:'}))
-
         .pipe(gulp.dest('./css/'))
-        .pipe(debug({title: 'gulp-debug:',minimal:false}))
         ;
 });
 
-
-
-
-gulp.task('jekyll', function() {
-    var plugins = [];
-  return spawn('jekyll', ['build'
-  ]);
-});
