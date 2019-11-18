475ba37d4d022f27edd05d9903f896f5068f2ce0
flxbot <flxbot@gmail.com>
2019-11-18 16:26:42 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('css', function() {
     return gulp.src(path.join(__dirname, "css/main.css")
         .pipe(postcss([
             uncss({
-                html: 'path.join(__dirname, "index.html")',
+                html: ['path.join(__dirname, "index.html")'],
                 htmlroot: '_site',
                 ignore: ['.green']
             }),
